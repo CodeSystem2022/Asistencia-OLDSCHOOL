4ae0ad3bf909ead72a8021420b4e87020fe85f7a
AndreaReynoso132 <95582384+AndreaReynoso132@users.noreply.github.com>
2022-10-20 10:12:17 -0300

    Asistencia Andrea

--- AsistenciaOctubre/src/AsistenciaOctubre.java
+++ AsistenciaOctubre/src/AsistenciaOctubre.java
@@ -65,7 +65,36 @@ public class AsistenciaOctubre {
     
     
     }
+             //ANDREA REYNOSO 
+             package ejerciciociclos07;
 
+             import javax.swing.JOptionPane;
+             
+             
+             public class EjercicioCiclos07 {
+             
+                
+                 public static void main(String[] args) {
+                  
+                     int numero, conteo = 0, suma = 0;
+                     float promedio = 0;
+                    
+                     numero = Integer.parseInt(JOptionPane.showInputDialog("Digite un número"));
+                     while(numero>=0){//mientras el numero no sea negativo
+                         suma += numero;
+                         conteo ++;
+                         numero = Integer.parseInt(JOptionPane.showInputDialog("Digite otro número"));
+                         
+                     }
+                     if (conteo==0){
+                        JOptionPane.showMessageDialog(null, "Error La division entre cero no existe");
+                     
+                     }
+                     else{
+                         promedio = (float)suma/conteo;
+                       
+                         JOptionPane.showMessageDialog(null, "El promedio es: "+promedio);
+                     }
 
 
 
