c24f560cdb6d1ca145eeff1ab5d9cfce7e14f86f
panchotoni <panchotonidandel97@gmail.com>
2022-10-24 16:52:58 -0300

    Correcciones

--- AsistenciaOctubre/src/AsistenciaOctubre.java
+++ AsistenciaOctubre/src/AsistenciaOctubre.java
@@ -13,92 +13,80 @@ public class AsistenciaOctubre {
         JOptionPane.showMessageDialog(null, "El factorial es " + factorial);
 
 
-
         //Ejercicio Yair Mathez.
 
         Scanner entrada = new Scanner(System.in);
         int numero, suma = 0;
-        for(int i=1; i<= 10; i++){
+        for (int i = 1; i <= 10; i++) {
             System.out.println("Digite un numero");
             numero = Integer.parseInt(entrada.nextLine());
             suma += numero;
 
         }
-        System.out.println("\nLa suma de todos los numeros es: "+suma);
-    
-    
+        System.out.println("\nLa suma de todos los numeros es: " + suma);
+
+
         //EJERCICIO JULIAN ANTEQUERA   
-    
+
         Scanner entrada2 = new Scanner(System.in);
-                float numero1;
+        float numero1;
 
-                System.out.println("Digite un numero");
-                numero1 = entrada2.nextFloat();
+        System.out.println("Digite un numero");
+        numero1 = entrada2.nextFloat();
 
-                float resultado = 0;
-                float acc = 0;
-                float suma2 = 0;
+        float resultado = 0;
+        float acc = 0;
+        float suma2 = 0;
 
-                while (numero1 > 0){;
-                    suma2 = suma2 + numero1;
-                    acc++;
-                    System.out.println("Digite un numero");
-                    numero1 = entrada2.nextFloat();
-                }
-                resultado = suma2 / acc;
-                System.out.println("El promedio es: " + resultado)
-    
-            //YESSICA REYNOSO
-            public class Ciclos06 {
-                public static void main(String[] arg) {
-                    Scanner entrada = new Scanner(System.in); 
-                    int numero,suma =0;
-                    do{
-                        System.out.println("Digite un numero:");
-                        numero = Integer.parseInt(entrada.nextLine());
-                        suma += numero;
-                        
-                    }while(numero !=0);
-                    System.out.println("La suma de todos los numeros que ingreso es de:"+suma);
-                }
-                
-    
-    
+        while (numero1 > 0) {
+            ;
+            suma2 = suma2 + numero1;
+            acc++;
+            System.out.println("Digite un numero");
+            numero1 = entrada2.nextFloat();
+        }
+        resultado = suma2 / acc;
+        System.out.println("El promedio es: " + resultado);
+
+        //YESSICA REYNOSO
+
+        Scanner entrada3 = new Scanner(System.in);
+        int numero2, suma3 = 0;
+        do {
+            System.out.println("Digite un numero:");
+            numero2 = Integer.parseInt(entrada3.nextLine());
+            suma3 += numero2;
+
+        } while (numero2 != 0);
+        System.out.println("La suma de todos los numeros que ingreso es de:" + suma3);
+
+
+        //ANDREA REYNOSO
+
+
+        int numero4, conteo = 0, suma4 = 0;
+        float promedio = 0;
+
+        numero4 = Integer.parseInt(JOptionPane.showInputDialog("Digite un número"));
+        while (numero4 >= 0) {//mientras el numero no sea negativo
+            suma4 += numero4;
+            conteo++;
+            numero4 = Integer.parseInt(JOptionPane.showInputDialog("Digite otro número"));
+
+        }
+        if (conteo == 0) {
+            JOptionPane.showMessageDialog(null, "Error La division entre cero no existe");
+
+        } else {
+            promedio = (float) suma4 / conteo;
+
+            JOptionPane.showMessageDialog(null, "El promedio es: " + promedio);
+        }
+
+
     }
-             //ANDREA REYNOSO 
-             package ejerciciociclos07;
-
-             import javax.swing.JOptionPane;
-             
-             
-             public class EjercicioCiclos07 {
-             
-                
-                 public static void main(String[] args) {
-                  
-                     int numero, conteo = 0, suma = 0;
-                     float promedio = 0;
-                    
-                     numero = Integer.parseInt(JOptionPane.showInputDialog("Digite un número"));
-                     while(numero>=0){//mientras el numero no sea negativo
-                         suma += numero;
-                         conteo ++;
-                         numero = Integer.parseInt(JOptionPane.showInputDialog("Digite otro número"));
-                         
-                     }
-                     if (conteo==0){
-                        JOptionPane.showMessageDialog(null, "Error La division entre cero no existe");
-                     
-                     }
-                     else{
-                         promedio = (float)suma/conteo;
-                       
-                         JOptionPane.showMessageDialog(null, "El promedio es: "+promedio);
-                     }
-
-
-
 }
+
 
 
 
