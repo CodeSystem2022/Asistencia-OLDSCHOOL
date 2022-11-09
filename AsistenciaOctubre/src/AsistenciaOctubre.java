57fd37194cd577e72f296a643e682acc3180e3b2
Tomascanete33 <tomascanete33@gmail.com>
2022-11-09 19:12:48 -0300

    Ejercicio Tomas Cañete

--- AsistenciaOctubre/src/AsistenciaOctubre.java
+++ AsistenciaOctubre/src/AsistenciaOctubre.java
@@ -1,8 +1,10 @@
+
 import javax.swing.*;
 import java.util.Scanner;
 
 //EJERCICIO FACTORIAL. FRANCISCO TONIDANDEL
 public class AsistenciaOctubre {
+
     public static void main(String[] args) {
         long factorial = 1;
         int num = Integer.parseInt(JOptionPane.showInputDialog("Digite un numero: "));
@@ -12,9 +14,7 @@ public class AsistenciaOctubre {
         }
         JOptionPane.showMessageDialog(null, "El factorial es " + factorial);
 
-
         //Ejercicio Yair Mathez.
-
         Scanner entrada = new Scanner(System.in);
         int numero, suma = 0;
         for (int i = 1; i <= 10; i++) {
@@ -25,9 +25,7 @@ public class AsistenciaOctubre {
         }
         System.out.println("\nLa suma de todos los numeros es: " + suma);
 
-
         //EJERCICIO JULIAN ANTEQUERA   
-
         Scanner entrada2 = new Scanner(System.in);
         float numero1;
 
@@ -49,7 +47,6 @@ public class AsistenciaOctubre {
         System.out.println("El promedio es: " + resultado);
 
         //YESSICA REYNOSO
-
         Scanner entrada3 = new Scanner(System.in);
         int numero2, suma3 = 0;
         do {
@@ -60,10 +57,7 @@ public class AsistenciaOctubre {
         } while (numero2 != 0);
         System.out.println("La suma de todos los numeros que ingreso es de:" + suma3);
 
-
         //ANDREA REYNOSO
-
-
         int numero4, conteo = 0, suma4 = 0;
         float promedio = 0;
 
@@ -84,20 +78,19 @@ public class AsistenciaOctubre {
         }
 
         //Ejercicio Gustavo Malaniuk
-
         long factorial15 = 1;
         int numero7 = Integer.parseInt(JOptionPane.showInputDialog("Digite un número: "));
         for (int i = 1; i <= numero7; i++) {
             factorial15 *= i;
         }
         JOptionPane.showMessageDialog(null, "El factorial del número ingresado es: " + factorial15);
+
+        // EJERCICIO TOMAS CA�ETE
+        long res1 = 1;
+        for (int a = 1; a <= 20; a += 2) {
+            res1 *= a;
+        }
+        JOptionPane.showMessageDialog(null, "Producto de numeros impares es: ");
     }
+
 }
-
-
-
-
-
-
-
-
